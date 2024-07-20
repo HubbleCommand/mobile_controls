a808e31430cba8adb53b0f9892de166e865d972e
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2024-07-20 20:03:22 +0200

    plugin remove floating joystick comments

--- addons/mobile_controls/mobile_controls.gd
+++ addons/mobile_controls/mobile_controls.gd
@@ -2,17 +2,13 @@
 extends EditorPlugin
 
 func _enter_tree() -> void:
-	# Green color taken from Control node
-	# Joystick icons based off of InputEventJoypadMotion (previously JoyAxis)
+	# Green color taken from Control node, joystick icons based off of InputEventJoypadMotion (previously JoyAxis)
 	add_custom_type("VirtualJoystick", "MarginContainer", preload("./components/virtual_joystick.gd"), preload("./icons/VirtualJoystick.svg"))
-	# Floating has background similar to MakeFloating
-	#add_custom_type("FloatingVirtualJoystick", "MarginContainer", preload("./components/floating_virtual_joystick.gd"), preload("./icons/FloatingVirtualJoystick.svg"))
 	
-	#Screen gesture based on InputEventScreen* phone parts, and hand taken from XRHandModifier3D
+	# Screen gesture based on InputEventScreen* phone parts, and hand taken from XRHandModifier3D
 	add_custom_type("ScreenGesture", "Control", preload("./components/screen_gesture.gd"), preload("./icons/ScreenGesture.svg"))
 
 
 func _exit_tree() -> void:
 	remove_custom_type("VirtualJoystick")
-	#remove_custom_type("FloatingVirtualJoystick")
 	remove_custom_type("ScreenGesture")
