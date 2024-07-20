0760d6d6df65c4aaf9e576369e3d6073482eeba1
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2024-07-20 13:56:45 +0200

    VirtualJoystick margin export

--- addons/mobile_controls/components/virtual_joystick.gd
+++ addons/mobile_controls/components/virtual_joystick.gd
@@ -3,6 +3,8 @@ extends MarginContainer
 #Images from https://godotengine.org/asset-library/asset/1787, code is mine
 class_name VirtualJoystick
 
+@export var margin: int = 20
+
 @export var outline_texture: Texture2D:
 	set(value):
 		outline_texture = value
@@ -42,10 +44,10 @@ enum EPointerConstraintMode { DYNAMIC_IN, DYNAMIC_OUT }
 
 func _ready():
 	#init this node for addon
-	add_theme_constant_override("margin_left", 20)
-	add_theme_constant_override("margin_right", 20)
-	add_theme_constant_override("margin_top", 20)
-	add_theme_constant_override("margin_bottom", 20)
+	add_theme_constant_override("margin_left", margin)
+	add_theme_constant_override("margin_right", margin)
+	add_theme_constant_override("margin_top", margin)
+	add_theme_constant_override("margin_bottom", margin)
 	
 	#Create scene for addon
 	outline = TextureRect.new()
