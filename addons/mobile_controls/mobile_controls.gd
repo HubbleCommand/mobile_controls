28c370507726676395515e5612e6035de1fc8d3b
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2024-07-22 13:19:55 +0200

    clean package gd

--- addons/mobile_controls/mobile_controls.gd
+++ addons/mobile_controls/mobile_controls.gd
@@ -2,10 +2,7 @@
 extends EditorPlugin
 
 func _enter_tree() -> void:
-	# Green color taken from Control node, joystick icons based off of InputEventJoypadMotion (previously JoyAxis)
 	add_custom_type("VirtualJoystick", "MarginContainer", preload("./components/virtual_joystick.gd"), preload("./icons/VirtualJoystick.svg"))
-	
-	# Screen gesture based on InputEventScreen* phone parts, and hand taken from XRHandModifier3D
 	add_custom_type("ScreenGesture", "Control", preload("./components/screen_gesture.gd"), preload("./icons/ScreenGesture.svg"))
 
 
