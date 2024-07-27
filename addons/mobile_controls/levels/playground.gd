2d912abd5f0bd18421257cefec22a0cf13efa255
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2024-07-27 21:24:05 +0200

    playground show joystick readout

--- addons/mobile_controls/levels/playground.gd
+++ addons/mobile_controls/levels/playground.gd
@@ -1,5 +1,32 @@
 extends Node2D
 
+@onready var joysticks = [
+	$CanvasLayer/VBoxContainer/ScreenGesture/FloatingVirtualJoystick,
+	$"CanvasLayer/VBoxContainer/DynamicOut Tests/AspectRatioContainer/LeftVirtualJoystick",
+	$"CanvasLayer/VBoxContainer/DynamicOut Tests/RightVirtualJoystick"
+]
+@onready var dropdown = $"CanvasLayer/Joystick Readout/OptionButton"
+
+func _ready() -> void:
+	dropdown.clear()
+	for joystick in joysticks:
+		dropdown.add_item(joystick.name)
+
+
+func _input(event: InputEvent) -> void:
+	if event is not InputEventJoypadMotion:
+		return
+	
+	if dropdown.selected > 0:
+		if event.axis == joysticks[dropdown.selected].joy_axis_horizontal:
+			$"CanvasLayer/Joystick Readout/XLabel".text = str(event.axis_value)
+		elif event.axis == joysticks[dropdown.selected].joy_axis_vertical:
+			$"CanvasLayer/Joystick Readout/YLabel".text = str(event.axis_value)
+	#if event.axis == JoyAxis.JOY_AXIS_LEFT_X || event.axis == JoyAxis.JOY_AXIS_RIGHT_X || event.axis == JoyAxis.JOY_AXIS_TRIGGER_LEFT :
+		
+	#else:
+		
+
 
 func _print_received_gesture(gesture: String):
 	print("		gesture received : ", gesture)
